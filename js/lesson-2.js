32d52a1f55c4c626fdca2bf6a2022109b1f80b44
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-06-05 12:54:30 +0300

    js_blended

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -1,43 +1,58 @@
 //!     Завдання 1:
+
 //? Створіть масив styles з елементами 'jazz' і 'blues'
 //? Додайте до кінця масиву елемент 'rock-n-roll' за допомогою відповідного методу масивів
 //? Знайдіть елемент 'blues' у масиві та замініть його на 'classic', використовуючи JavaScript-код
 
-// Напишіть функцію logItems(array), яка приймає масив як аргумент
-// і виводить у консоль кожен його елемент у форматі:
-// "<номер елемента> - <значення елемента>".
-// Використайте цикл for для перебору елементів масиву.
-// Нумерація елементів повинна починатися з 1 (а не з 0).
+//? Напишіть функцію logItems(array), яка приймає масив як аргумент
+//? і виводить у консоль кожен його елемент у форматі:
+//? "<номер елемента> - <значення елемента>".
+//? Використайте цикл for для перебору елементів масиву.
+//? Нумерація елементів повинна починатися з 1 (а не з 0).
 
-const styles = ["jazz", "blues"];
-styles.push("rock-n-roll");
-// styles[1] = "classic";
-const index = styles.indexOf("blues");
-if (index === -1) {
-  console.log("Not found");
-} else {
-  styles[index] = "classic";
-}
-// console.log(styles);
+//* const styles = ["jazz", "blues"];
+//* styles.push("rock-n-roll");
+//* // styles[1] = "classic";
+//* const index = styles.indexOf("blues");
+//* if (index === -1) {
+//*   console.log("Not found");
+//* } else {
+//*   styles[index] = "classic";
+//* }
+//*
 
-function logItems(array) {
-  for (let i = 0; i < array.length; i++) {
-    console.log(i);
-}
+//* function logItems(array) {
+//*   for (let i = 0; i < array.length; i++) {
+//*     console.log(`${i + 1} - ${array[i]}`);
+//*   }
+//* }
+//* logItems(styles);
 
-console.log();
-
+//* for (const arr of styles) {
+//*   console.log(arr);
+//* }
 //!_______________________________________________________________________
 
 //!      Завдання 2:
-// Напишіть функцію checkLogin(array), яка:
-// Приймає масив логінів як аргумент.
-// Запитує ім'я користувача через prompt.
-// Перевіряє, чи є введене ім'я у переданому масиві.
-// Якщо ім'я є в масиві – виводить повідомлення через alert: "Welcome, <name>!"
-// Якщо ім'я відсутнє – виводить повідомлення: "User not found".
+//? Напишіть функцію checkLogin(array), яка:
+//? Приймає масив логінів як аргумент.
+//? Запитує ім'я користувача через prompt.
+//? Перевіряє, чи є введене ім'я у переданому масиві.
+//? Якщо ім'я є в масиві – виводить повідомлення через alert: "Welcome, <name>!"
+//? Якщо ім'я відсутнє – виводить повідомлення: "User not found".
 
-// const logins = ["Peter", "John", "Igor", "Sasha"];
+const logins = ["Peter", "John", "Igor", "Sasha"];
+
+function checkLogin(array) {
+  const name = prompt("Введіть ваше ім'я");
+  if (array.includes(name)) {
+    alert(`Welcome, ${name}!`);
+  } else {
+    alert("User not found");
+  }
+}
+checkLogin(logins);
+
 //!_______________________________________________________________________
 
 //!      Завдання 3:
