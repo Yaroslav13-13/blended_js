2f2c57fa93271e265bcd4461b314e2f70adf3046
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-07-27 22:58:23 +0300

    js_blended

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -1,56 +1,75 @@
 //! 1 - отримай body елемент і виведи його в консоль;
 //* const body = document.querySelector("body");
 //* console.log(body);
+
 //! 2 - отримай елемент id="title" і виведи його в консоль;
 //* const title = document.querySelector("#title");
 //* console.log(title);
+
 //! 3 - отримай елемент class="list" і виведи його в консоль;
 //* const list = document.querySelector(".list");
 //* console.log(list);
+
 //! 4 - отримай всі елементи з атрибутом data-topic і виведи їх в консоль;
 //* const dataTopic = document.querySelectorAll("[data-topic]");
 //* dataTopic.forEach((el) => {
 //*   console.log(el.dataset.topic);
 //* });
+
 //! 5 - отримай перший елемент з списку всіх елементів з атрибутом data-topic і виведи його в консоль;
 //* const dataTopic = document.querySelectorAll("[data-topic]");
 //* console.log(dataTopic[0]);
+
 //! 6 - отримай останній елемент з списку всіх елементів з атрибутом data-topic і виведи його в консоль;
 //* const dataTopic = document.querySelectorAll("[data-topic]");
 //* const lastElement = dataTopic[dataTopic.length - 1];
 //* console.log(lastElement);
+
 //! 7 - який елемент є сусідом для h1? Знайти і виведи його в консоль;
 //* const h1Element = document.querySelector("h1");
 //* console.log(h1Element.nextElementSibling);
+
 //! 8 - по тегу h3 знайти всі заголовки та виведи їх у консоль;
 //* const titleH3 = document.querySelectorAll("h3");
 //* titleH3.forEach((el) => {
 //*   console.log(el.textContent);
 //* });
+
 //! 9 - для кожного елмента h3 додай class="active", який змінить колір заголовка на червоний колір
 const titleH3 = document.querySelectorAll("h3");
 titleH3.forEach((el) => {
   el.classList.add("active");
   el.style.color = "red";
 });
-//! 10 - знайти елемент li який має атрибут data-topic з значенням "navigation" і виведи його в консоль;
-// const navItem = document.querySelectorAll("li[data-topic]");
-// navItem.forEach((el) => {
-//   if (el.getAttribute("data-topic") === "navigation") {
-//     console.log(el);
-//   }
-// });
 
-// const navItemm = document.querySelector('li[data-topic="navigation"]');
-// console.log(navItemm);
+//! 10 - знайти елемент li який має атрибут data-topic з значенням "navigation" і виведи його в консоль;
+//* const navItem = document.querySelectorAll("li[data-topic]");
+//* navItem.forEach((el) => {
+//*   if (el.getAttribute("data-topic") === "navigation") {
+//*     console.log(el);
+//*   }
+//* });
+//* const navItemm = document.querySelector('li[data-topic="navigation"]');
+//* console.log(navItemm);
 
 //! 11 - додай для знайденого елемента data-topic="navigation" атрибут style і зроби його backgroundColor жовтим
 const navItem = document.querySelector('li[data-topic="navigation"]');
 navItem.setAttribute("style", "background-color:yellow");
+
 //! 12 - у елемента data-topic="navigation" знайди елемент р і зміни його текст на "Я змінив тут текст!".
+const paragraph = document.querySelector('[data-topic="navigation"] p');
+paragraph.textContent = "Я змінив тут текст!";
+
 //! 13 - створи const currentTopic = "manipulation"; після цього знайди елемент у якогоо атрибут data-topic має значення, яке зберігається у змінній currentTopic і виведи його в консоль;
+const currentTopic = "manipulation";
+const topic = document.querySelector(`li[data-topic="${currentTopic}"]`);
+console.log(topic);
+
 //! 14 - додай до знайденого елемента атрибут style і зроби його backgroundColor блакитним;
+topic.setAttribute("style", "background-color:lightblue");
+
 //! 15 - знайти в документі заголовок, який має class="completed" і виведи його в консоль;
+
 //! 16 - видали елемент li в якому знаходиться заголовок, який має class="completed"
 //! 17 - після заголовка h1 (перед списком) додай новий елемент p і задай йому наступний текст: "Об'єктна модель документа (Document Object Model)"
 //! 18 - додай новий елемент списку у кінець списка, його заголовок це - "Властивість innerHTML" а опис (р) - "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу". тобто, потрібно створити елемент LI потім наповнити H3 та P і готову LI закинути у кінець списку
