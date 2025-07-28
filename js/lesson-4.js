d58fa7986dfd0f85c714fc7dec53b3c4b4a2767e
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-07-28 13:02:36 +0300

    js_blended

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -121,7 +121,7 @@ list.insertAdjacentHTML("beforeend", newItem);
 
 const numberContainer = document.querySelector(".number-container");
 const randomNumber = () => Math.floor(Math.random() * 100) + 1;
-
+//! Варіант 1
 //* for (let i = 0; i < 100; i++) {
 //*   const numberDiv = document.createElement("div");
 //*   const num = randomNumber();
@@ -136,11 +136,38 @@ const randomNumber = () => Math.floor(Math.random() * 100) + 1;
 //* }
 
 //! Варіант 2
-
 const blocks = Array.from({ length: 100 }).map(() => {
   const num = randomNumber();
   const className = num % 2 === 0 ? "even" : "old";
   return `<div class="number ${className}">${num}</div>`;
 });
-
 numberContainer.innerHTML = blocks.join("");
+
+//todo ===========================   Task - 3   ====================================
+//! Form Events, Input, Focus, Blur and Submit.
+
+//! Використовуй шаблон форми з файлу html.
+
+//! 1 - При події `input`, якщо користувач ввів в поле більше
+//! 6 символів то додати клас `success`. Якщо ж символів менше аніж 6,
+//! то клас `error`
+
+// 2 - При події `focus` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
+// якщо при фокусі поле непусте, то `outline` => `'3px solid green'`
+
+// 3 - При події `blur` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
+// якщо при фокусі поле непусте, то `outline` => `'3px solid lime'`
+
+// 4 - При події `submit`. Відміни поведінку браузера по змовчуванню.
+// Дістань данні з інпуту і чек боксу, зроби перевірку,
+// що інпут не порожній, також, що нажатий чек бокс у положення true,
+// якщо користувач все виконав вірно, збери данні (userName)
+// у обьект і виведи у консоль. У разі, якщо користувач не виконав
+// одну із умов, виведи повідомлення. Також при події інпут реалізуй додавання
+// ім`я користувача у span, замість слова "Anonymous".
+// Якщо користувач ввів ім`я, а потім видалив, зроби так,
+// щоб на місце повернулось дефолтне знаяення "Anonymous".
+// При відправці форми, очисти інпут, верни чек бокс у положення
+// false, верни дефолтне значення "Anonymous" у span.
